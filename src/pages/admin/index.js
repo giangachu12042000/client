10fdb5e02dcc2bebf01336be28d1204ec019e1ca
su ho <subhawj95@gmail.com>
2020-11-05 22:21:35 +0700

    dasboad

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import {Layout} from 'antd'
-import GetRoute from '../../app/getRoute'
-
+import React from 'react';
+import './style.scss';
+import {Layout} from 'antd';
+import GetRoute from '../../app/getRoute';
+import {Link} from 'react-router-dom';
 const { Header, Content, Footer, Sider } = Layout;
 
 const Addmin =(props)=>{
     const {routes} = props;
     return(
       <Layout>
-      <Sider>Sider</Sider>
+          <div className="contain">
+            <div>
+              <div className="avatar">
+
+              </div>
+            </div>
+            <Sider className="sider" >
+                <div className="contain-name">
+                  <Link className="list-name-item" to="/admin/user">user</Link>
+                </div>
+                <div className="contain-name">
+                  <Link className="list-name-item" to="/admin/product">product</Link>
+                </div>
+                <div className="contain-name">
+                  <Link className="list-name-item" to="/admin/category">category</Link>
+                </div>
+            </Sider>
+          </div>
       <Layout>
         <Header>Header</Header>
         <Content>
